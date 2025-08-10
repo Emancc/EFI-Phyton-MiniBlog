c8b3d477ae5b22da1e09e4126a39490de05515d1
Emancc <e.cepedacaceres@itecriocuarto.org.ar>
2025-08-09 23:38:43 -0300

    funciones para registro y logueo mas redirecciones

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from models import db, Users
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mi_clave_secreta'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db.init_app(app)
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('login'))
+
 
 @app.route('/')
 def index():
@@ -8,3 +37,56 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        if request.method == 'POST':
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+
+            user_exist = Users.query.filter((Users.username == username) | (Users.email == email)).first()
+            if user_exist:
+                flash('El usuario o email ya existe', 'danger')
+                return redirect(url_for('register'))
+
+            new_user = Users(username=username, email=email)
+            new_user.set_password(password)
+
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registro exitoso, ya puedes iniciar sesión', 'success')
+            return redirect(url_for('login'))
+
+        return render_template('auth/register.html')
+
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            user = Users.query.filter_by(username=username).first()
+            if user and user.check_password(password):
+                login_user(user)
+                flash('Has iniciado sesión correctamente', 'success')
+                next_page = request.args.get('next')
+                return redirect(next_page or url_for('index'))
+            else:
+                flash('Usuario o contraseña incorrectos', 'danger')
+                return redirect(url_for('login'))
+
+        return render_template('auth/login.html')
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        flash('Has cerrado sesión correctamente', 'info')
+        return redirect(url_for('login'))
